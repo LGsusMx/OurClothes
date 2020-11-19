b4f1b19ef9cdeaf83df448a0b2dac13f505b334a
LGsus Rmz <caednicolas2@gmail.com>
2020-11-18 18:22:27 -0600

    [Add] Upload product function

--- dist/js/index.js
+++ dist/js/index.js
@@ -10,3 +10,28 @@ db.collection('usuarios').doc(idusuario).get().then( doc =>{
     datosdelacuenta.innerHTML = html;
 
 });
+function guardarProducto(){
+    var idusuariA = sessionStorage.getItem("idusuario");
+    //var idusuariA = 'testinguwu';
+    const file = document.querySelector('#photo').files[0];
+    const metadata ={
+        contentType:file.type
+    };
+    const task = ref.child(document.getElementById('txtNombrePro').value + idusuariA).put(file,metadata);
+    task.then(snapshot => snapshot.ref.getDownloadURL()).then(url =>{
+        db.collection("productos").add({
+            Descripción: document.getElementById('txtDescripcionPro').value,
+            IDUsuario: idusuariA,
+            Imagen: url,
+            NombreArticulo: document.getElementById('txtNombrePro').value,
+            Precio: parseFloat(document.getElementById('txtPrecioPro').value),
+            Status:'Disponible'
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+    })
+}
